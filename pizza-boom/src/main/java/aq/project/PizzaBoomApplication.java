0bf8a401457105b2671a49cf5b1447441b0ef139
U-235-92 <a745296@yandex.ru>
2025-07-20 21:39:27 +0400

    Added empty project pizza-boom

---
+++ pizza-boom/src/main/java/aq/project/PizzaBoomApplication.java
@@ -0,0 +1,13 @@
+package aq.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaBoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaBoomApplication.class, args);
+	}
+
+}
---
+++ pizza-boom/src/test/java/aq/project/PizzaBoomApplicationTests.java
@@ -0,0 +1,13 @@
+package aq.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaBoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
