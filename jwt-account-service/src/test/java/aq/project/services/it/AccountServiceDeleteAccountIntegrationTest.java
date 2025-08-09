0bc2aaeea8ad6edd0e30c717d040c13cf0f0ebd0
U-235-92 <a745296@yandex.ru>
2025-08-09 23:38:56 +0400

    Add delete account integration tests

---
+++ jwt-account-service/src/test/java/aq/project/services/it/AccountServiceDeleteAccountIntegrationTest.java
@@ -0,0 +1,64 @@
+package aq.project.services.it;
+
+import static aq.project.util.AccountLogins.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import aq.project.entities.Account;
+import aq.project.entities.Role;
+import aq.project.exceptions.AccountNotFoundException;
+import aq.project.repository.AccountRepository;
+import aq.project.services.AccountService;
+
+@SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)
+class AccountServiceDeleteAccountIntegrationTest {
+
+	@Autowired
+	private AccountService accountService;
+	@MockitoBean
+	private AccountRepository accountRepository;
+
+	@BeforeAll
+	private void initRepository() {
+		Account johnAcc = new Account(JOHN, "1", Role.ADMIN); johnAcc.setId(1);
+		Account sarahAcc = new Account(SARAH, "2", Role.EDITOR); sarahAcc.setId(2);
+		Account junkoAcc = new Account(JUNKO, "3", Role.READER); junkoAcc.setId(3);
+		when(accountRepository.findByLogin(null)).thenThrow(NullPointerException.class);
+		doReturn(Optional.of(johnAcc)).when(accountRepository).findByLogin(JOHN);
+		doReturn(Optional.of(sarahAcc)).when(accountRepository).findByLogin(SARAH);
+		doReturn(Optional.of(junkoAcc)).when(accountRepository).findByLogin(JUNKO);
+	}
+	
+	@Test
+	@DisplayName("test-success-account-delete")
+	void test1() {
+		accountService.deleteAccount(SARAH);
+		verify(accountRepository).deleteByLogin(SARAH);
+	}
+	
+	@Test
+	@DisplayName("test-null-account-delete")
+	void test2() {
+		assertThrows(NullPointerException.class, () -> accountService.deleteAccount(null));
+	}
+	
+	@Test
+	@DisplayName("test-not-found-account-delete")
+	void test3() {
+		assertThrows(AccountNotFoundException.class, () -> accountService.deleteAccount(NOT_FOUND_LOGIN));
+	}
+}
