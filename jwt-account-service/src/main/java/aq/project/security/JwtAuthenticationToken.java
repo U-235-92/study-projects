dfc5d8402ceae39ea2cfff679648f9bef974a246
U-235-92 <a745296@yandex.ru>
2025-08-12 16:42:06 +0400

    Authentication token adjusted

--- jwt-account-service/src/main/java/aq/project/security/JwtAuthenticationToken.java
+++ jwt-account-service/src/main/java/aq/project/security/JwtAuthenticationToken.java
@@ -1,60 +1,59 @@
 package aq.project.security;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import io.jsonwebtoken.Claims;
 
 public class JwtAuthenticationToken implements Authentication {
 
-	private Claims claims;
+	private static final long serialVersionUID = 1L;
+	
+	private String login;
+	private boolean isBlocked;
+	private List<? extends GrantedAuthority> roles;
 	
 	public JwtAuthenticationToken(Claims claims) {
-		this.claims = claims;
+		roles = List.of(new SimpleGrantedAuthority(claims.get("role").toString()));
+		login = claims.getSubject();
 	}
 	
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return login;
 	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		return roles;
 	}
 
 	@Override
 	public Object getCredentials() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Object getDetails() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Object getPrincipal() {
-		// TODO Auto-generated method stub
-		return null;
+		return login;
 	}
 
 	@Override
 	public boolean isAuthenticated() {
-		// TODO Auto-generated method stub
-		return false;
+		return isBlocked;
 	}
 
 	@Override
 	public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-
+		isBlocked = isAuthenticated;
 	}
-
 }
