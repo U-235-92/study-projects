104155f5c1c31411a22636dd416e6c19726a70c7
U-235-92 <a745296@yandex.ru>
2025-08-18 11:05:55 +0400

    Added empty project

---
+++ aq-authorization-service/src/main/java/aq/project/AqAuthorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package aq.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqAuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqAuthorizationServiceApplication.class, args);
+	}
+
+}
