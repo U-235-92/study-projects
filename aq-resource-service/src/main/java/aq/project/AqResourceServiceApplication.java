1e5df215e6eb2125d7375ab5b8b1b6685ef6e60d
U-235-92 <a745296@yandex.ru>
2025-08-23 16:03:33 +0400

    Added empty project

---
+++ aq-resource-service/src/main/java/aq/project/AqResourceServiceApplication.java
@@ -0,0 +1,13 @@
+package aq.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqResourceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqResourceServiceApplication.class, args);
+	}
+
+}
---
+++ aq-resource-service/src/test/java/aq/project/AqResourceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aq.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqResourceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
