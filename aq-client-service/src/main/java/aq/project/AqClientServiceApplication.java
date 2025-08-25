b99b13bd9610c4c485ebd2d9cdb1fcc3c2ee7b39
U-235-92 <a745296@yandex.ru>
2025-08-25 13:40:04 +0400

    Added empty project

---
+++ aq-client-service/src/main/java/aq/project/AqClientServiceApplication.java
@@ -0,0 +1,13 @@
+package aq.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqClientServiceApplication.class, args);
+	}
+
+}
