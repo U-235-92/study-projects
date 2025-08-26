7e3f4a1b7adb5d2df592699b8917f94ad3f0393a
U-235-92 <a745296@yandex.ru>
2025-08-26 15:19:11 +0400

    Added jasypt handling

--- aq-client-service/src/main/java/aq/project/AqClientServiceApplication.java
+++ aq-client-service/src/main/java/aq/project/AqClientServiceApplication.java
@@ -3,7 +3,10 @@ package aq.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class AqClientServiceApplication {
 
 	public static void main(String[] args) {
