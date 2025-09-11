c0121242dfabaee870854ba7dd976ca0c386546b
U-235-92 <a745296@yandex.ru>
2025-09-11 14:50:01 +0400

    Modify report service

--- k-report-service/src/main/java/aq/project/ReportServiceMain.java
+++ k-report-service/src/main/java/aq/project/ReportServiceMain.java
@@ -6,14 +6,36 @@ import java.time.ZoneId;
 
 import aq.project.service.ReportService;
 
-public class Main {
+public class ReportServiceMain {
 
 	public static void main(String[] args) {
 		ReportService reportService = new ReportService();
+//		reportService.printProductsAsStream();
+//		allocateProductsByColor(reportService);
+//		reportService.printCountProductNumberByPeriod(getFromMills(), getDuration());
+		reportService.printTotalProductNumber("BOW-5588");
+//		reportService.printTotalProductNumber("RDE-8085");
+//		reportService.printTotalProductNumber("BHJ-5050");
+	}
+	
+	@SuppressWarnings("unused")
+	private static void allocateProductsByColor(ReportService reportService) {
 		reportService.allocateByColor("RED");
 		reportService.allocateByColor("BLUE");
 		reportService.allocateByColor("BLACK");
-		reportService.printCountProductNumberByPeriod(LocalDateTime.now().minusDays(1).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli(), Duration.ofHours(1));
-		reportService.printTotalProductNumber("ABC-585");
+	}
+	
+	@SuppressWarnings("unused")
+	private static long getFromMills() {
+		return LocalDateTime.now()
+				.minusDays(1)
+				.atZone(ZoneId.systemDefault())
+				.toInstant()
+				.toEpochMilli();
+	}
+	
+	@SuppressWarnings("unused")
+	private static Duration getDuration() {
+		return Duration.ofHours(1);
 	}
 }
--- k-report-service/src/main/java/aq/project/service/ReportService.java
+++ k-report-service/src/main/java/aq/project/service/ReportService.java
@@ -2,6 +2,7 @@ package aq.project.service;
 
 import java.time.Duration;
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -11,61 +12,99 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.ValueMapper;
 
-import aq.project.util.serialize.JsonDeserializer;
-import aq.project.util.serialize.JsonSerializer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import aq.project.dto.ProductDto;
 
 public class ReportService {
 	
-	private Properties properties;
+	private static final String COLOR_PRODUCTS_TOPIC = "color-products";
+	private static final String COMPLETE_PRODUCTS_TOPIC = "complete-products";
+	
 	private Executor executor;
+	private Properties properties;
+	private ObjectMapper objectMapper;
+	private ValueMapper<String, ProductDto> valueMapper;	
 	
 	public ReportService() {
 		properties = new Properties();
 		properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:5050");
-		properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "report-service");
-		executor = Executors.newFixedThreadPool(3);
+		properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "report-service#" + UUID.randomUUID().toString());
+		properties.put("auto.offset.reset", "earliest");
+		executor = Executors.newCachedThreadPool();
+		objectMapper = new ObjectMapper();
+		valueMapper = value -> {
+			try {
+				return objectMapper.readValue(value, ProductDto.class);
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+			return null;
+		};
 	}
 	
+	@SuppressWarnings("resource")
+	public  void printProductsAsStream() {
+		executor.execute(() -> {
+			StreamsBuilder streamsBuilder = new StreamsBuilder();
+			streamsBuilder.stream(COMPLETE_PRODUCTS_TOPIC, Consumed.with(Serdes.String(), Serdes.String()))
+				.mapValues(v -> valueMapper.apply(v))
+				.filter((k, v) -> v != null)
+				.foreach((k, v) -> System.out.println(v));
+			KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
+			kafkaStreams.start();
+		});
+	}
+	
+	@SuppressWarnings("resource")
 	public void printTotalProductNumber(String name) {
 		executor.execute(() -> {
 			StreamsBuilder streamsBuilder = new StreamsBuilder();
-			streamsBuilder.stream("complete-products", Consumed.with(Serdes.String(), Serdes.serdeFrom(new JsonSerializer(), new JsonDeserializer())))
-				.filter((k, v) -> v.getName().equals(name))
+			streamsBuilder.stream(COMPLETE_PRODUCTS_TOPIC, Consumed.with(Serdes.String(), Serdes.String()))
+				.mapValues(v -> valueMapper.apply(v))
+				.filter((k, v) -> v != null && v.getName().equals(name))
+				.peek((key, value) -> System.out.println(key))
 				.groupByKey()
 				.count()
 				.toStream()
 				.foreach((k, v) -> System.out.println(k + ": " + v));
-			@SuppressWarnings("resource")
 			KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
 			kafkaStreams.start();
 		});
 	}
 	
+	@SuppressWarnings("resource")
 	public void allocateByColor(String color) {
 		executor.execute(() -> {			
 			StreamsBuilder streamsBuilder = new StreamsBuilder();
-			streamsBuilder.stream("complete-products", Consumed.with(Serdes.String(), Serdes.serdeFrom(new JsonSerializer(), new JsonDeserializer())))
-				.filter((k, v) -> v.getColor().equals(color))
+			streamsBuilder.stream(COMPLETE_PRODUCTS_TOPIC, Consumed.with(Serdes.String(), Serdes.String()))
+				.mapValues(v -> valueMapper.apply(v))
+				.filter((k, v) ->  v != null && v.getColor().equals(color))
 				.selectKey((k, v) -> v.getColor())
-				.to("color-products");
-			@SuppressWarnings("resource")
+				.to(COLOR_PRODUCTS_TOPIC);
 			KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
 			kafkaStreams.start();
 		});
 	}
 	
+	@SuppressWarnings("resource")
 	public void printCountProductNumberByPeriod(long from, Duration duration) {
 		executor.execute(() -> {			
 			StreamsBuilder streamsBuilder = new StreamsBuilder();
-			streamsBuilder.stream("complete-products", Consumed.with(Serdes.String(), Serdes.serdeFrom(new JsonSerializer(), new JsonDeserializer())))
-				.filter((k, v) -> v.getCreatedAt() >= from)
+			streamsBuilder.stream(COMPLETE_PRODUCTS_TOPIC, Consumed.with(Serdes.String(), Serdes.String()))
+				.mapValues(v -> valueMapper.apply(v))
+				.filter((k, v) -> v != null && v.getCreatedAt() >= from)
 				.groupBy((k, v) -> v.getColor())
 				.windowedBy(TimeWindows.ofSizeWithNoGrace(duration))
 				.count()
 				.toStream()
 				.foreach((k, v) -> System.out.println(k + ": " + v));
-			@SuppressWarnings("resource")
 			KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
 			kafkaStreams.start();
 		});
